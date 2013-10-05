8c9a7511bb896efc4678316f4797b80914129df4
Garen Torikian <gjtorikian@gmail.com>
2013-10-05 15:42:19 -0700

    Add cmake_osx_sysroot

--- build-libgit2.sh
+++ build-libgit2.sh
@@ -6,5 +6,6 @@ mkdir build
 cd build
 cmake .. -DCMAKE_C_FLAGS="$1" -DCMAKE_BUILD_TYPE=Release -DTHREADSAFE=1 \
          -DBUILD_CLAR=0 -DBUILD_SHARED_LIBS=0 -DZLIB_LIBRARY=0 \
-         -DSHA1_TYPE=builtin -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8
+         -DSHA1_TYPE=builtin -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 \
+         -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
 cmake --build .
